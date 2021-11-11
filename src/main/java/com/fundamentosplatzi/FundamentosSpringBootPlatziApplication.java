7003f6a03e06d0e81d99e8a9a1bfa9f5260411b6
Martin.Albarracin <me.albarracin@globant.com>
2021-11-11 09:39:38 -0500

    first commit version, project structure

---
+++ src/main/java/com/fundamentosplatzi/FundamentosSpringBootPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringBootPlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringBootPlatziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/FundamentosSpringBootPlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringBootPlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
